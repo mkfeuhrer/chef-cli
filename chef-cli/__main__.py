e9d811500bec0813359a3f7c765c722f962bd11e
Mohit Khare <mohitfeuhrer@gmail.com>
2018-09-05 15:59:55 +0530

    Added institution , contests and contest detail in parser

--- chef-cli/__main__.py
+++ chef-cli/__main__.py
@@ -7,6 +7,12 @@ def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--user', '-u', required=False, metavar='<Username>',
                         help='Get user information.')
+    parser.add_argument('--institution', '-i', required=False, metavar='<Institution>',
+                        help='Institution Filter. Eg: "Motilal Nehru National Institute of Technology"')
+    parser.add_argument('--contests', required=False, action='store_true',
+                        help='Get All Contests')
+    parser.add_argument('--contestdetails', required=False, metavar='<ContestDetails>',
+                        help='Get details of Contest.Eg- JAN17,APR18')
     return parser
 
 
@@ -22,11 +28,28 @@ def main(argv=None):
         parser = create_parser()
         args = parser.parse_args(argv[1:])
 
+        # Arguments initialization
         user = args.user
+        institution = args.institution
+        contests = args.contests
+        contestDetails=args.contestdetails
 
+        # Parser check
         if user:
             # Make request to fetch user details .....
             print(makeRequest("GET", "https://api.codechef.com/users/"+user).content)
+
+        elif institution:
+            # Make request to search institution
+            print(makeRequest("GET", "https://api.codechef.com/institution?search="+institution).content)
+
+        elif contests:
+            # Make request to fetch list of all contests
+            print(makeRequest("GET", "https://api.codechef.com/contests").content)
+
+        elif contestDetails:
+            # Make request to fetch details of particular contest
+            print(makeRequest("GET", "https://api.codechef.com/contests/"+contestDetails).content)
 
     except KeyboardInterrupt:
         print('\nBye.')
